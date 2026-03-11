070e6a11b682648b41526453130c84fb73ec4e51
luciano <luciano.canuto@outlook.com>
2026-03-11 18:03:48 -0300

    feat: CadastroCliente Concluido

--- app/src/main/java/com/lucianocanuto/comprefacil/PRESENTATION/viewmodel/CadastroViewModel.kt
+++ app/src/main/java/com/lucianocanuto/comprefacil/PRESENTATION/viewmodel/CadastroViewModel.kt
@@ -1,4 +1,38 @@
 package com.lucianocanuto.comprefacil.PRESENTATION.viewmodel
 
-class CadastroViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.lucianocanuto.comprefacil.DATA.repository.EnderecoRepository
+import com.lucianocanuto.comprefacil.DOMAIN.model.Endereco
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+@HiltViewModel
+class CadastroViewModel @Inject constructor(
+    private val enderecoRepository: EnderecoRepository
+) : ViewModel() {
+
+    private val enderecoObservavel = MutableLiveData<Endereco>()
+    val endereco: LiveData<Endereco> = enderecoObservavel
+
+    fun buscarCep(cep: String) {
+
+        viewModelScope.launch {
+
+            try {
+
+                val buscarEndereco = enderecoRepository.buscarCep(cep)
+                enderecoObservavel.value = buscarEndereco
+
+            } catch (e: Exception) {
+
+            }
+
+        }
+    }
+
 }
