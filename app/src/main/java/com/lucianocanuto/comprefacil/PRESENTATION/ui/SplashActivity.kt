bf2a5665729e7e28086493adb397f8f0571dc0eb
luciano <luciano.canuto@outlook.com>
2026-03-12 20:00:53 -0300

    feat: Splash Add

--- app/src/main/java/com/lucianocanuto/comprefacil/PRESENTATION/ui/SplashActivity.kt
+++ app/src/main/java/com/lucianocanuto/comprefacil/PRESENTATION/ui/SplashActivity.kt
@@ -1,21 +1,41 @@
 package com.lucianocanuto.comprefacil.PRESENTATION.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.lucianocanuto.comprefacil.R
+import com.lucianocanuto.comprefacil.UTIL.CompreFacilLogo
+import com.lucianocanuto.comprefacil.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivitySplashBinding.inflate(layoutInflater)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(binding.root)
+
+        binding.txtSplash.text = CompreFacilLogo()
+
+        binding.txtSplash.animate()
+            .alpha(1f)
+            .scaleX(1.8f)
+            .scaleY(1.8f)
+            .setDuration(3200)
+            .start()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(
+                Intent(this, MainActivity::class.java)
+            )
+            finish()
+        }, 3000 )
+
     }
 }
